3602fe5a9932f4dbc2287c8b28d930fa9debcd87
Wesley <cptwesley@jojoweba.nl>
2018-05-21 16:01:31 +0200

    Add ResourceDoesNotExistException tests

---
+++ CoreResourceManager.Tests/src/Exceptions/ResourceDoesNotExistExceptionTests.cs
@@ -0,0 +1,20 @@
+﻿using CoreResourceManager.Exceptions;
+using Xunit;
+
+namespace Librio.Api.Detector.Test.Exceptions
+{
+    /// <summary>
+    /// Test class for the <see cref="ResourceDoesNotExistException"/> class.
+    /// </summary>
+    public class ResourceDoesNotExistExceptionTests
+    {
+        /// <summary>
+        /// Tests that the message constructor does have the correct message.
+        /// </summary>
+        [Fact]
+        public void MessagedConstructorTest()
+        {
+            Assert.Equal("bla", new ResourceDoesNotExistException("bla").Message);
+        }
+    }
+}
