11e35de31fc252652194d8bb7d167599110c4aca
Wesley <cptwesley@jojoweba.nl>
2018-05-21 16:01:15 +0200

    Add ResourceDoesNotExistException class

---
+++ CoreResourceManager/src/Exceptions/ResourceDoesNotExistException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CoreResourceManager.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when the stream does not represent an image.
+    /// </summary>
+    /// <seealso cref="Exception" />
+    public class ResourceDoesNotExistException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceDoesNotExistException"/> class.
+        /// </summary>
+        public ResourceDoesNotExistException()
+            : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceDoesNotExistException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        public ResourceDoesNotExistException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceDoesNotExistException"/> class.
+        /// </summary>
+        /// <param name="message">The exception message.</param>
+        /// <param name="innerException">The inner exception.</param>
+        public ResourceDoesNotExistException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
