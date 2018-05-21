42e33f2d3bf00d0e0f0df5c378b1ee9fbdb45720
Wesley <cptwesley@jojoweba.nl>
2018-05-21 16:17:18 +0200

    Add tests

--- CoreResourceManager.Tests/src/ResourcesTests.cs
+++ CoreResourceManager.Tests/src/ResourcesTests.cs
@@ -1,11 +1,32 @@
-﻿using Xunit;
+﻿using System.IO;
+using CoreResourceManager.Exceptions;
+using Xunit;
 
 namespace CoreResourceManager.Tests
 {
     /// <summary>
     /// Test class for the <see cref="Resources"/> class.
     /// </summary>
-    public class ResourcesTests
+    public static class ResourcesTests
     {
+        /// <summary>
+        /// Checks that the resource is retrieved correctly.
+        /// </summary>
+        [Fact]
+        public static void GetStreamTest()
+        {
+            Stream stream = Resources.Get("demo.txt");
+            StreamReader reader = new StreamReader(stream);
+            Assert.Equal("hello world", reader.ReadToEnd());
+        }
+
+        /// <summary>
+        /// Checks that the resource is retrieved correctly.
+        /// </summary>
+        [Fact]
+        public static void InvalidResourceTest()
+        {
+            Assert.Throws<ResourceDoesNotExistException>(() => Resources.Get("2fw464"));
+        }
     }
 }
